b25f1b2c5a0d9ccc777b834b99c5ef1f87804054
Rafael Martins Nascimento <108582966+Rafinha003@users.noreply.github.com>
2024-08-13 19:27:09 -0300

    Add files via upload

---
+++ ProgramacaoPHP/2_conceitos_basicos/6_palavras_reservadas/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  // function echo() {
+  //   return true;
+  // }
+
+  // function while() {
+  //   return true;
+  // }
+  $a = [1,2,3];
+
+  print_r($a);
+
+  function whileNew() {
+    return true;
+  }
